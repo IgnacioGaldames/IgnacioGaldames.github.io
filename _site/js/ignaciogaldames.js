52bed923c99a434483996be5f2c6c9babd14e7ab
Ignacio Galdames <caminantestelar@gmail.com>
2017-03-01 10:35:20 -0300

    añadir material

--- _site/js/ignaciogaldames.js
+++ _site/js/ignaciogaldames.js
@@ -5,22 +5,3 @@ $(document).ready(function() {
   var pathname = window.location.pathname;
   $('.navbar-nav > li > a[href="' + pathname + '"]').parent().addClass('active');
 });
-
-var feed = new Instafeed({
-  clientId: '02e0b4818b424ec385636cc92137d822',
-  accessToken: '179507307.ba4c844.79a1e62709614e39b166cda514ce829d',
-  target: 'instafeed',
-  template: '<div class="bg" style="background-image:url({{image}})"></div>',
-  get: 'user',
-  userId: '179507307',
-  resolution: 'standard_resolution',
-  inks: false,
-  limit: 1,
-  success: function() {
-    console.log('success');
-  },
-  after: function() {
-    console.log('after');
-  },
-});
-feed.run();
