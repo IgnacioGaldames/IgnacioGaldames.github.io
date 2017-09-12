2d704efc90457a732cf9bf0736144beefba5bdea
Ignacio Galdames <caminantestelar@gmail.com>
2017-09-12 18:30:13 -0300

    cambios en aside

--- js/ignaciogaldames.js
+++ js/ignaciogaldames.js
@@ -1,32 +1,30 @@
 console.log('Cargando');
 
 $(document).ready(function() {
-  console.log("Bienvenido");
-  var pathname = window.location.pathname;
-  $('.navbar-nav > li > a[href="' + pathname + '"]').parent().addClass('active');
+    console.log("Bienvenido");
+    var pathname = window.location.pathname;
+    $('.navbar-nav > li > a[href="' + pathname + '"]').parent().addClass('active');
 
-  $( ".nav-item" ).hover(function() {
-    $('.dropdown-toggle').dropdown();
-  });
-  
+    $(".nav-item").hover(function() {
+        $('.dropdown-toggle').dropdown();
+    });
 
-
-$('#scene').parallax();
-$('#scene').parallax({
-  clipRelativeInput: true,
-  calibrateX: false,
-  calibrateY: true,
-  invertX: false,
-  invertY: true,
-  limitX: false,
-  limitY: 10,
-  scalarX: 2,
-  scalarY: 10,
-  frictionX: 0.2,
-  frictionY: 0.8,
-  originX: 0.0,
-  originY: 1.0
-});
+    $('#scene').parallax();
+    $('#scene').parallax({
+        clipRelativeInput: true,
+        calibrateX: false,
+        calibrateY: true,
+        invertX: false,
+        invertY: true,
+        limitX: false,
+        limitY: 10,
+        scalarX: 2,
+        scalarY: 10,
+        frictionX: 0.2,
+        frictionY: 0.8,
+        originX: 0.0,
+        originY: 1.0
+    });
 
     //Funciones
 
@@ -34,5 +32,22 @@ $('#scene').parallax({
         $('#glitch').text('miTitulo');
     }
     theGlitch();
-});
+    wow = new WOW({
+        boxClass: 'wow', // default
+        animateClass: 'animated', // default
+        offset: 0, // default
+        mobile: true, // default
+        live: true // default
+    })
+    wow.init();
 
+    WebFont.load({
+        google: {
+            families: ['Roboto Slab', 'Source Code Pro', 'Source Sans Pro', 'Source Serif Pro', 'EB Garamond']
+        }
+    });
+
+    particlesJS.load('particles-js', '/js/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+});
