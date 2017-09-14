0a46c5567fecfdf5871b627b5a39f594d9398737
Ignacio Galdames <caminantestelar@gmail.com>
2017-09-14 10:29:51 -0300

    gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('hello', function() {
+    console.log('Hello Ignacio');
+});
+
+gulp.task('sass', function() {
+    return gulp.src('sass/ignaciogaldames.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('css'))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('sass/ignaciogaldames.sass', ['sass']);
+    // Other watchers
+})
+
+gulp.task('default', ['watch'], function() {
+
+})
