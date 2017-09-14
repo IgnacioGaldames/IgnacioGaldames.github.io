ba6e6cfe7e23e873578dbd9810fb908e2291c670
Ignacio Galdames <caminantestelar@gmail.com>
2017-09-14 12:12:30 -0300

    gulp + jekyll + browserSync
    Añadir gulp para vigilar cambios, luego ejecutar sass, luego jekyll y luego ejecutar un browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+
+const child = require('child_process');
 
 gulp.task('hello', function() {
     console.log('Hello Ignacio');
@@ -9,15 +12,32 @@ gulp.task('hello', function() {
 gulp.task('sass', function() {
     return gulp.src('sass/ignaciogaldames.sass')
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(sourcemaps.write('css'))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
-gulp.task('watch', function() {
-    gulp.watch('sass/ignaciogaldames.sass', ['sass']);
+gulp.task('jekyll', function() {
+    const jekyll = child.spawn('jekyll', ['build']);
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: '_site',
+            port: 4000
+        },
+    })
+});
+
+gulp.task('watch', ['sass', 'jekyll', 'browserSync'], function() {
+    gulp.watch('sass/ignaciogaldames.sass', ['sass','jekyll']);
     // Other watchers
-})
+    gulp.watch('_site/*', browserSync.reload);
+});
 
 gulp.task('default', ['watch'], function() {
 
