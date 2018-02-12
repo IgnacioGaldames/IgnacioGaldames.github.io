252de98099fd7f34c697c0ea790957ecf026b84b
IgnacioGaldames-Cardumen <ignacio.galdames@cardumen.cl>
2018-02-12 16:49:34 -0300

    basic jekyll

--- _source/_libros/tiempos_alterados.md
+++
@@ -1,8 +0,0 @@
----
-layout: template_libros
-title: Tiempos Alterados
-text: Cuentos sobre percepciones temporales
-img: cover_tiempos_alterados.jpg
----
-
-Una recopilación de cuentos de ciencia ficción sobre percepciones temporales. Desde una distopia futurista hasta un mundo solo de robots.
--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,10 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var sourcemaps = require('gulp-sourcemaps');
-var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
 
-const child = require('child_process');
+gulp.task('default', defaultTask);
 
-gulp.task('hello', function() {
-    console.log('Hello Ignacio');
-});
+function defaultTask(done) {
+  // place code for your default task here
+console.log('hello');
 
-gulp.task('useref'
-, function(){
-    return gulp.src('_site/*.html')
-      .pipe(useref())
-      // Minifies only if it's a JavaScript file
-      .pipe(gulpIf('*.js', uglify()))
-      .pipe(gulp.dest('_site'))
-});
-
-gulp.task('sass', function() {
-    return gulp.src('sass/ignaciogaldames.sass')
-        .pipe(sourcemaps.init())
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
-
-gulp.task('jekyll', function() {
-    const jekyll = child.spawn('jekyll', ['build','--watch', '--incremental'])
-    return gulp.src('_site')
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
-
-gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: '_site',
-            port: 4000
-        },
-    })
-});
-
-gulp.task('watch', ['sass', 'jekyll', 'browserSync'], function() {
-    gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], ['sass','jekyll']);
-    // Other watchers
-    gulp.watch(['_source/**/*.html'], ['sass','jekyll']);
-    gulp.watch(['_source/**/*.js'], ['sass','jekyll']);
-    gulp.watch('_site/**/*.*').on('change', browserSync.reload);
-});
-
-gulp.task('default', ['watch'], function() {
-
-})
+  done();
+}
